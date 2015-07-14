3d0d92787b68e5970da4b6c5ae8cfac8e5f49968
ridermansb <ridermansb@gmail.com>
2015-07-14 16:42:33 -0300

    Modify app

--- app.js
+++ app.js
@@ -6,24 +6,42 @@ var dayInfo;
 var idx = 0;
 var canStartProcess = false;
 
+var sumHours = function(start1, end1, start2, end2) {
+    var diff1 = end1.diff(start1);
+    var diff2 = end2.diff(start2);
+    return {
+        days: diff1.days + diff2.days,
+        hours: diff1.hours + diff2.hours,
+        minutes: diff1.minutes + diff2.minutes
+    };
+}
+
 var isHour = function(text) {
-    var isH = text === "0013" || text === "0001";
-    return isH;
+    return text === "0013" || text === "0001";
 };
 
-var extractDay = function(itemText) {
-    itemText = itemText.replace('/', '//');
-    var hoursExtracted = /(.*)(Horas.*)/gmi.exec(itemText);
+var mapDate = function(month, day, year, hours) {
+    return hours.map(function(obj){
+        return new Date(month + '/' + day + '/' + year + ' ' + obj);
+    });
+}
+
+var extractDay = function(hoursText) {
+    hoursText = hoursText.replace('/', '//').trim();
+    var hoursExtracted = /(.*)(Horas.*)/gmi.exec(hoursText);
     if (hoursExtracted && hoursExtracted.length > 0) {
-        if (hoursExtracted[1].trim()) {
+        hoursText = hoursExtracted[1].trim();
+        hoursList = mapDate(1, dayInfo.day, 2015, hoursText.split(' '));
+        if (hoursText) {
             return {
                 day: dayInfo.day,
                 dayMonth: dayInfo.dayMonth,
-                hours: hoursExtracted[1].trim()
+                hours: hoursList,
+                totalWork: sumHours(hoursList[0], hoursList[1], hoursList[2], hoursList[3])
             };
         }
     } else {
-        console.log("Hours not extracted from '%s'", itemText);
+        console.log("Hours not extracted from '%s'", hoursText);
     }
 
     return undefined;
@@ -45,10 +63,53 @@ new PdfReader().parseFileItems("sample.pdf", function(err, item) {
         var extractedDay = extractDay(item.text);
         if (extractedDay) {
             hours.push(extractedDay)
-            console.log(extractedDay);
+            console.log("On day %s, you work %s:%s", extractedDay.day, extractedDay.totalWork.hours, extractedDay.totalWork.minutes);
         }
         dayInfo = undefined;
     }
 
     idx++;
 });
+
+ function DateDiff(date1, date2) {
+    this.days = null;
+    this.hours = null;
+    this.minutes = null;
+    this.seconds = null;
+    this.date1 = date1;
+    this.date2 = date2;
+
+    this.init();
+  }
+
+  DateDiff.prototype.init = function() {
+    var data = new DateMeasure(this.date1 - this.date2);
+    this.days = data.days;
+    this.hours = data.hours;
+    this.minutes = data.minutes;
+    this.seconds = data.seconds;
+  };
+
+  function DateMeasure(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    s = s % 60;
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+
+    this.days = d;
+    this.hours = h;
+    this.minutes = m;
+    this.seconds = s;
+  };
+
+  Date.diff = function(date1, date2) {
+    return new DateDiff(date1, date2);
+  };
+
+  Date.prototype.diff = function(date2) {
+    return new DateDiff(this, date2);
+  };
