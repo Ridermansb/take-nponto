cc533d6a6d84f1c390de2cc94db88729084593e3
ridermansb <ridermansb@gmail.com>
2015-09-18 09:07:59 -0300

    Fix same issues for code complexity

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ var dayInfo;
 var idx = 0;
 var canStartProcess = false;
 
+var DateMeasure = function(ms) {
+    var d, h, m, s;
+    s = Math.floor(ms / 1000);
+    m = Math.floor(s / 60);
+    s = s % 60;
+    h = Math.floor(m / 60);
+    m = m % 60;
+    d = Math.floor(h / 24);
+    h = h % 24;
+
+    this.days = d;
+    this.hours = h;
+    this.minutes = m;
+    this.seconds = s;
+};
+
 var sumHours = function (start1, end1, start2, end2) {
-    let diff1 = end1.diff(start1);
-    let diff2 = end2.diff(start2);
-    let minutes = (diff1.hours + diff2.hours) * 60;
+    var diff1 = end1.diff(start1);
+    var diff2 = end2.diff(start2);
+    var minutes = (diff1.hours + diff2.hours) * 60;
     return minutes + diff1.minutes + diff2.minutes;
 };
 
@@ -43,7 +59,21 @@ var extractDays = function (hoursText) {
     return undefined;
 };
 
+var callback = function (hours) {
+    var totalMinutes = 0;
+    hours.forEach(function (it) {
+        totalMinutes += it.totalWork;
+        console.log("On day %s, you work %s:%s", it.day, Math.floor(it.totalWork / 60), Math.round(it.totalWork % 60));
+    });
+
+    var hoursWorked = Math.floor(totalMinutes / 60);
+    var minutesWorked = Math.round(totalMinutes % 60);
+
+    console.log("Total : %d:%d", hoursWorked, minutesWorked);
+};
+
 var hours = [];
+
 new PdfReader().parseFileItems(argv._[0], function (err, item) {
     if (!item) callback(hours);
 
@@ -57,7 +87,7 @@ new PdfReader().parseFileItems(argv._[0], function (err, item) {
         dayInfo = {
             day: itemsPriors[idx - 2].text,
             dayMonth: itemsPriors[idx - 1].text
-        }
+        };
     } else if (dayInfo) {
         var extractedDay = extractDays(item.text);
         if (extractedDay) {
@@ -88,22 +118,6 @@ DateDiff.prototype.init = function () {
     this.seconds = data.seconds;
 };
 
-function DateMeasure(ms) {
-    var d, h, m, s;
-    s = Math.floor(ms / 1000);
-    m = Math.floor(s / 60);
-    s = s % 60;
-    h = Math.floor(m / 60);
-    m = m % 60;
-    d = Math.floor(h / 24);
-    h = h % 24;
-
-    this.days = d;
-    this.hours = h;
-    this.minutes = m;
-    this.seconds = s;
-};
-
 Date.diff = function (date1, date2) {
     return new DateDiff(date1, date2);
 };
@@ -111,16 +125,3 @@ Date.diff = function (date1, date2) {
 Date.prototype.diff = function (date2) {
     return new DateDiff(this, date2);
 };
-
-var callback = function (hours) {
-    var totalMinutes = 0;
-    hours.forEach(function (it) {
-        totalMinutes += it.totalWork
-        console.log("On day %s, you work %s:%s", it.day, Math.floor(it.totalWork / 60), Math.round(it.totalWork % 60));
-    });
-
-    var hoursWorked = Math.floor(totalMinutes / 60);
-    var minutesWorked = Math.round(totalMinutes % 60);
-
-    console.log("Total : %d:%d", hoursWorked, minutesWorked);
-}
